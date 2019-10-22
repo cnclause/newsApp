0f3ef1ab5add2a46e7ad25d36a1cfc2eefac0317
github username <github email address>
2019-10-22 12:09:10 -0600

    added post, get for users

--- news_api/api/users.js
+++ news_api/api/users.js
@@ -2,10 +2,30 @@ const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
 
-module.exports =[ 
-    router.get('/', (req, res) => {
-        queries.getAll().then(users => {
-            res.json(users)
-        })
+function validUser(user){
+    const hasUsername = typeof user.username == 'string' && user.username.trim() != ''
+    const hasPassword = typeof user.password == 'string' && user.password.trim() != ''
+    return hasUsername && hasPassword
+}
+
+
+
+
+router.get('/', (req, res) => {
+    queries.getAll().then(users => {
+        res.json(users)
     })
-]
+}),
+
+router.post('/', (req, res, next) => {
+    if(validUser(req.body)) {
+        queries.createUser(req.body).then(users => {
+            res.json(users[0])
+        })
+    } else {
+        next(new Error ('invalid Username'))
+    }
+})
+
+
+module.exports = router
